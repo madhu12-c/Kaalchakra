e181931e75bc4d1220a6aba9d0122c2dd60822f3
madhu12-c <mchanda2006@gmail.com>
2026-01-24 18:40:46 +0530

    Backend Login working

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,27 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// connect database
+connectDB();
+
+// middlewares
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/protected", require("./routes/protected"));
+
+
+// test route
 app.get("/", (req, res) => {
-  res.send("Backend running");
+  res.send("Kaalchakra backend running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
